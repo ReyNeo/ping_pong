01fde4c176a586da21cffbefa2ff57a84e7eac50
Character <artemkov004@gmail.com>
2025-11-28 13:02:21 +0700

    added counter
    добавлен счётчик, теперь можно понять кто выиграл а кто лох

--- ping_pong.py
+++ ping_pong.py
@@ -1,14 +1,16 @@
 from pygame import *
-from helper import GameSprite, GameRectangle
+from helper import GameRectangle
 from pygame.sprite import collide_rect
+font.init()
 					#x   y
-window_resolution = (900,500)
+window_resolution = (1000,500)
 FPS = 60
 speed = 5
 window = display.set_mode(window_resolution)
 display.set_caption("ping pong")
 clock = time.Clock()
-
+player_1_counter = 0
+player_2_counter = 0
 pong_y_speed = speed
 pong_x_speed = speed
 
@@ -16,13 +18,17 @@ background = GameRectangle (0,0,window_resolution[0],window_resolution[1],(0,0,0
 player_1 = GameRectangle(40,100,10,100,(255,255,255))
 player_2 = GameRectangle(window_resolution[0]-50,100,10,100,(255,255,255))
 pong = GameRectangle(400,300,10,10,(255,255,255))
+counter_font = font.Font(None,36)
 
 MAINGAMELOOP = True
 while MAINGAMELOOP:
 	background.update(window)
 	player_1.update(window)
 	player_2.update(window)
-	
+	txt_player_1_counter = counter_font.render(str(player_1_counter),1,(255,255,255))
+	window.blit(txt_player_1_counter,(window_resolution[0]//2 -30,5))
+	txt_player_2_counter = counter_font.render(str(player_2_counter),1,(255,255,255))
+	window.blit(txt_player_2_counter,(window_resolution[0]//2 +30,5))
 	pong.y += pong_y_speed
 	pong.x += pong_x_speed
 	if pong.y < 0: pong_y_speed = speed
@@ -36,9 +42,12 @@ while MAINGAMELOOP:
 		pong_x_speed = -speed
 
 	if pong.x < 0 or pong.x > window_resolution[0]:
+		if pong.x < 0: player_1_counter +=1 
+		elif pong.x > window_resolution[0]: player_2_counter += 1
 		pong.x = window_resolution[0] // 2
 		pong.y = window_resolution[1] // 2
 		pong_x_speed = -pong_x_speed
+
 
 	for e in event.get():
 		if e.type == QUIT:
